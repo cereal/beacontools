ed8e4f392b74110bc7a0dfb15f0147d61e3cc1f8
Kai Oliver Quambusch <cereal@users.noreply.github.com>
2018-12-03 22:38:41 +0100

    save a call to str.lower
    and we don't have to care about invalid inputs, yay ... maybe i should add the "useless" test back :P

--- beacontools/device_filters.py
+++ beacontools/device_filters.py
@@ -80,6 +80,10 @@ class BtAddrFilter(DeviceFilter):
     def __init__(self, bt_addr):
         """Initialize filter."""
         super(BtAddrFilter, self).__init__()
-        if bt_addr is None or not is_valid_mac(bt_addr):
+        try:
+            bt_addr = bt_addr.lower()
+        except AttributeError:
+            raise ValueError("bt_addr({}) wasn't a string".format(bt_addr))
+        if not is_valid_mac(bt_addr):
             raise ValueError("Invalid bluetooth MAC address given, format should match aa:bb:cc:dd:ee:ff")
-        self.properties['bt_addr'] = bt_addr.lower()
+        self.properties['bt_addr'] = bt_addr
