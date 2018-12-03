67d9de8f6ebb04429e9cb574cc25d710f49228ad
Kai Oliver Quambusch <cereal@users.noreply.github.com>
2018-12-03 22:03:54 +0100

    fixed cyclic import

--- beacontools/utils.py
+++ beacontools/utils.py
@@ -5,7 +5,6 @@ import array
 import struct
 
 from .const import ScannerMode
-from .device_filters import IBeaconFilter, EddystoneFilter, BtAddrFilter, EstimoteFilter
 
 # compiled regex, ignores case
 RE_MAC_ADDR = compile_regex('(?:[0-9a-f]{2}:){5}(?:[0-9a-f]{2})', I)
@@ -14,18 +13,22 @@ RE_MAC_ADDR = compile_regex('(?:[0-9a-f]{2}:){5}(?:[0-9a-f]{2})', I)
 def is_valid_mac(mac):
     return RE_MAC_ADDR.match(mac) is not None
 
+
 def data_to_hexstring(data):
     """Convert an array of binary data to the hex representation as a string."""
     return hexlify(data_to_binstring(data)).decode('ascii')
+
 
 def data_to_uuid(data):
     """Convert an array of binary data to the iBeacon uuid format."""
     string = data_to_hexstring(data)
     return string[0:8]+'-'+string[8:12]+'-'+string[12:16]+'-'+string[16:20]+'-'+string[20:32]
 
+
 def data_to_binstring(data):
     """Convert an array of binary data to a binary string."""
     return array.array('B', data).tostring()
+
 
 def bt_addr_to_string(addr):
     """Convert a binary string to the hex representation."""
@@ -35,12 +38,14 @@ def bt_addr_to_string(addr):
     # insert ":" seperator between the bytes
     return ':'.join(a+b for a, b in zip(hex_str[::2], hex_str[1::2]))
 
+
 def is_one_of(obj, types):
     """Return true iff obj is an instance of one of the types."""
     for type_ in types:
         if isinstance(obj, type_):
             return True
     return False
+
 
 def is_packet_type(cls):
     """Check if class is one the packet types."""
@@ -52,12 +57,14 @@ def is_packet_type(cls):
                     EddystoneTLMFrame, EddystoneEIDFrame, IBeaconAdvertisement, \
                     EstimoteTelemetryFrameA, EstimoteTelemetryFrameB])
 
+
 def to_int(string):
     """Convert a one element byte string to int for python 2 support."""
     if isinstance(string, str):
         return ord(string[0])
     else:
         return string
+
 
 def bin_to_int(string):
     """Convert a one element byte string to signed int for python 2 support."""
@@ -66,8 +73,10 @@ def bin_to_int(string):
     else:
         return struct.unpack("b", bytes([string]))[0]
 
+
 def get_mode(device_filter):
     """Determine which beacons the scanner should look for."""
+    from .device_filters import IBeaconFilter, EddystoneFilter, BtAddrFilter, EstimoteFilter
     if device_filter is None or len(device_filter) == 0:
         return ScannerMode.MODE_ALL
 
