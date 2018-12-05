6c7d5b41b81c8094c023dd7972f09e2aa714aa58
Kai Oliver Quambusch <cereal@users.noreply.github.com>
2018-12-05 23:49:00 +0100

    enhancement of LE_SET_SCAN_PARAMETERS call
    - some documentation on what we are doing in the scanner.py:Monitor.set_scan_parameters call

--- beacontools/const.py
+++ beacontools/const.py
@@ -10,6 +10,29 @@ class ScannerMode(IntEnum):
     MODE_ESTIMOTE = 4
     MODE_ALL = MODE_IBEACON | MODE_EDDYSTONE | MODE_ESTIMOTE
 
+
+# hci le scan parameters
+class ScanType(IntEnum):
+    """Determines which type of scan should be executed."""
+    PASSIVE = 0x00
+    ACTIVE = 0x01
+
+
+class ScanFilter(IntEnum):
+    """Determines if only white-listed MAC addresses will be filtered or not"""
+    ALL = 0x00
+    WHITELIST_ONLY = 0x01
+
+
+class BluetoothAddressType(IntEnum):
+    """Determines the scanner MAC-address"""
+    PUBLIC = 0x00  # with device MAC-address
+    RANDOM = 0x01  # with a random MAC-address
+
+
+# used for window and interval (i.e. 0x10 * 0.625 = 10ms)
+MS_FRACTION_MULTIPLIER = 0.625
+
 LE_META_EVENT = 0x3e
 OGF_LE_CTL = 0x08
 OCF_LE_SET_SCAN_PARAMETERS = 0x000B
