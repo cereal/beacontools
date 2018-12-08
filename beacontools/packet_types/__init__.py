548e0c678e53c243c46184fb9e7d86080f637063
Kai Oliver Quambusch <obscurity0072@gmail.com>
2018-12-08 01:47:25 +0100

    linter

--- beacontools/packet_types/__init__.py
+++ beacontools/packet_types/__init__.py
@@ -1,6 +1,6 @@
 """Packets supported by the parser."""
 from .packet import BasePacket
 from .eddystone import (EddystoneUIDFrame, EddystoneURLFrame, EddystoneEncryptedTLMFrame,
-                       EddystoneTLMFrame, EddystoneEIDFrame,)
+                        EddystoneTLMFrame, EddystoneEIDFrame,)
 from .ibeacon import IBeaconAdvertisement
 from .estimote import EstimoteTelemetryFrameA, EstimoteTelemetryFrameB
