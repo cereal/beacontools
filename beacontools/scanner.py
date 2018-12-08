ed61b3d5c6cd6dceeac8f7758f1e76322fbed1ad
Kai Oliver Quambusch <obscurity0072@gmail.com>
2018-12-08 03:38:44 +0100

    Impl __init__, run, stop, set_scan_parameters

--- beacontools/scanner.py
+++ beacontools/scanner.py
@@ -2,6 +2,7 @@
 import threading
 import struct
 import logging
+import contextlib
 from importlib import import_module
 
 from .parser import parse_packet
@@ -45,6 +46,83 @@ class Scanner(threading.Thread):
         self._bt_device_id = bt_device_id
         self._device_filter = Scanner.validate_filters(device_filter, DeviceFilter)
         self._packet_filter = Scanner.validate_filters(packet_filter, BasePacket)
+        self._btlib = import_module("bluetooth._bluetooth")  # bluez
+        self._socket = None
+        self._mode = get_mode(self._device_filter)
+        self._eddystone_mappings = []
+        # events
+        self._consuming = threading.Event()
+        self._resourceless = threading.Event()
+        self._resourceless.set()
+
+    def run(self):
+        """Continously scan for BLE advertisements."""
+        self._consuming.set()
+        with contextlib.closing(self._btlib.hci_open_dev(self._bt_device_id)) as socket:
+            self._socket = socket
+            self._resourceless.clear()
+
+            fltr = self._btlib.hci_filter_new()
+            self._btlib.hci_filter_all_events(fltr)
+            self._btlib.hci_filter_set_ptype(fltr, self._btlib.HCI_EVENT_PKT)
+            self._socket.setsockopt(self._btlib.SOL_HCI, self._btlib.HCI_FILTER, fltr)
+
+            self.set_scan_parameters()
+            self.toggle_scan(True)
+
+            while self._consuming.is_set():
+                pkt = self.socket.recv(255)
+                event = to_int(pkt[1])
+                subevent = to_int(pkt[3])
+                if event == LE_META_EVENT and subevent == EVT_LE_ADVERTISING_REPORT:
+                    # we have an BLE advertisement
+                    self.process_packet(pkt)
+            self.toggle_scan(False)
+            self._socket = None
+            self._resourceless.set()
+
+    def stop(self, blocking=False):
+        self._consuming.clear()
+        if blocking:
+            self._resourceless.wait()
+
+    def set_scan_parameters(self, scan_type=ScanType.ACTIVE, interval_ms=10, window_ms=10,
+                            mac_type=BluetoothAddressType.RANDOM, filter_type=ScanFilter.ALL):
+        """"sets the le scan parameters
+            type     - ScanType.(PASSIVE|ACTIVE)
+            interval - ms between scans (valid range 2.5ms - 10240ms)
+                  !note: when interval and window are equal, the scan runs continuos
+            window   - ms scan duration (valid range 2.5ms - 10240ms)
+            own_type - Bluetooth address type BluetoothAddressType.(PUBLIC|RANDOM)
+                       PUBLIC = use device Bluetooth MAC address
+                       RANDOM = generate and use a random MAC address
+            filter   - ScanFilter.(ALL|WHITELIST_ONLY) only ALL is supported, which will
+                       return all fetched bluetooth packets (WHITELIST_ONLY is not supported,
+                       because OCF_LE_ADD_DEVICE_TO_WHITE_LIST command is not implemented)"""
+        interval_fractions = interval_ms / MS_FRACTION_DIVIDER
+        if interval_fractions < 0x0004 or interval_fractions > 0x4000:
+            raise ValueError(
+                "Invalid interval given {}, must be in range of 2.5ms to 10240ms!".format(
+                    interval_fractions))
+
+        window_fractions = window_ms / MS_FRACTION_DIVIDER
+        if window_fractions < 0x0004 or window_fractions > 0x4000:
+            raise ValueError(
+                "Invalid window given {}, must be in range of 2.5ms to 10240ms!".format(
+                    window_fractions))
+
+        scan_parameter_pkg = struct.pack(
+            ">BHHBB",
+            scan_type,
+            interval_fractions,
+            window_fractions,
+            mac_type,
+            filter_type)
+        if not self._resouceless.is_set():
+            self._btlib.hci_send_cmd(self._socket, OGF_LE_CTL, OCF_LE_SET_SCAN_PARAMETERS,
+                                     scan_parameter_pkg)
+            return
+        raise RuntimeError("Couldn't send hci command, seems we are resourceless")
 
 
 class BeaconScanner(object):
